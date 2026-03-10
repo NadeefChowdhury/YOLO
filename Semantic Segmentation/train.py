4c8e356896f7eea1e66510b9a3b90baa1e0c769d
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2026-03-10 20:10:27 +0600

    Update train.py

--- Semantic Segmentation/train.py
+++ Semantic Segmentation/train.py
@@ -3,5 +3,6 @@ from ultralytics import YOLO
 
 
 if __name__ == "__main__":
-    model = YOLO('yolo26n-seg.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
+
     model.train(data="config.yaml", epochs=150, imgsz=640)
